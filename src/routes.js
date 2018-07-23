41e2da1207f4995663c74aabceb6295b33add544
Kim, Andrew (AssetMark) <Andrew.Kim@AssetMark.com>
2018-07-23 16:20:26 -0700

    Halfway in module 11

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from "./component/App";
 import HomePage from "./component/home/HomePage";
 import AboutPage from "./component/about/AboutPage";
 import CoursesPage from "./component/course/CoursesPage";
+import ManageCoursePage from "./component/course/ManageCoursePage";
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="courses" component={CoursesPage} />
+        <Route path="course" component={ManageCoursePage} />
+        <Route path="course/:id" component={ManageCoursePage} />
         <Route path="about" component={AboutPage} />
     </Route>
 );
