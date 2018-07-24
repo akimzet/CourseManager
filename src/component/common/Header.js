0f1ec5fba301471bc32d19bc11f4c0ae4113eedb
Kim, Andrew (AssetMark) <Andrew.Kim@AssetMark.com>
2018-07-24 10:23:10 -0700

    finished to module 13

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,7 +1,9 @@
 import React, { PropTypes } from "react";
 import { Link, IndexLink } from "react-router";
+import LoadingDots from './LoadingDots';
+import App from "../App";
 
-const Header = () => {
+const Header = ({loading}) => {
     return (
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -9,8 +11,19 @@ const Header = () => {
             <Link to="/courses" activeClassName="active">Courses</Link>
             {" | "}
             <Link to="/about" activeClassName="active">About</Link>
+            {loading && <LoadingDots interval={100} dots={20}/>}
         </nav>
     );
 };
+
+Header.propTypes = {
+    loading: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        loading: state.ajaxCallsInProgress > 0
+    };
+}
 
 export default Header;
